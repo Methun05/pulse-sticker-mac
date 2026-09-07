51e0c90c1c73be20e316d6ac1ff31de75da557df
Methun05 <mkspam0504@gmail.com>
2026-09-07 18:07:22 +0530

    Revert app/page.tsx to original

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,6 @@ import { SpotCardGrid } from '@/components/spots/SpotCardGrid';
 import { BidModal } from '@/components/spots/BidModal';
 import { HowItWorksSection } from '@/components/sections/HowItWorksSection';
 import { FAQSection } from '@/components/sections/FAQSection';
-import { PulseChainHeroBackground } from '@/components/background-gradient/dark-arc-bands-background';
 
 export default function HomePage() {
   const [spots, setSpots] = useState<SpotData[]>([]);
@@ -57,16 +56,14 @@ export default function HomePage() {
 
   return (
     <main className="min-h-screen bg-white">
-      {/* Dark hero with PulseChain gradient */}
-      <PulseChainHeroBackground>
-        <Navbar onBidClick={handleOpenGeneralBid} totalRaised={totalRaised} />
-        <HeroSection
-          onBidClick={handleOpenGeneralBid}
-          onExploreSpots={scrollToSpots}
-          totalRaised={totalRaised}
-          occupiedCount={occupiedCount}
-        />
-      </PulseChainHeroBackground>
+      <Navbar onBidClick={handleOpenGeneralBid} totalRaised={totalRaised} />
+
+      <HeroSection
+        onBidClick={handleOpenGeneralBid}
+        onExploreSpots={scrollToSpots}
+        totalRaised={totalRaised}
+        occupiedCount={occupiedCount}
+      />
 
       {/* MacBook mockup */}
       <section className="pb-4 sm:pb-8">
